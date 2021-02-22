e397e2e7e6f1115fed88a0528c697221b1a2a52e
yenlik <190103460@stu.sdu.edu.kz>
2021-02-23 05:14:01 +0600

    first commit

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>LAB4</title>
+    </head>
+
+    <body>
+        <div>
+            <h1 style="text-align: center;">Yenlik Akhatova</h1>
+        </div>
+    </body>
+
+</html>
